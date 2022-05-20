82d7c47ce423f8da541836669025067f79915a1b
Oybek-uzb <78894838+Oybek-uzb@users.noreply.github.com>
2022-05-20 16:54:17 +0500

    #added token types

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDTO } from './dto/auth.dto';
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class AuthService {
     constructor(private prismaService: PrismaService) {}
 
-    signupLocal(dto: AuthDTO) {
+    hashData(data: string) {
+        return bcrypt.hash(data, 10)
+    }
+
+    async signupLocal(dto: AuthDTO) {
+        const hash = await this.hashData(dto.password)
+        
         const newUser = this.prismaService.user.create({
             data: {
                 email: dto.email,
-                hash: dto.password
+                hash
             }
         })
     }
